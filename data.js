0c79598a682f4e8102e268f93ce462d373a003a2
Ümmügülsüm Can <36399161+ummugulsumcan@users.noreply.github.com>
2021-09-16 14:40:18 +0300

    Add files via upload
    initial commit

---
+++ data.js
@@ -0,0 +1,198 @@
+module.exports = function () {
+  return {
+    products: [
+      {id: 1, name: 'Samsung S5', price: 1000, imageUrl: '1.jpg', des: 'idare'},
+      {id: 2, name: 'Semsung S6', price: 2000, imageUrl: '2.jpg', des: 'iyi'},
+      {id: 3, name: 'Samsung S7', price: 3000, imageUrl: '3.jpg', des: 'kötü'},
+      {id: 4, name: 'Apple S8', price: 4000, imageUrl: '4.jpg', des: 'idare'}
+    ],
+    categories: [
+      {id: 4, name: 'Telefon'},
+      {id: 4, name: 'Bilgisayar'},
+      {id: 4, name: 'Elektronik'}
+    ],
+    orders: [],
+    data: [
+      {
+        _id: "60e313c9a6f42c23f0ffd216",
+        index: 0,
+        guid: "91c5dccd-074f-4fa0-9f0c-b3f37fd2ce44",
+        isActive: true,
+        balance: "$3,200.43",
+        picture: "https://picsum.photos/900/600",
+        age: 35,
+        eyeColor: "brown",
+        name: "Ramirez Marsh",
+        gender: "male",
+        company: "EXOSIS",
+        email: "ramirezmarsh@exosis.com",
+        phone: "+1 (963) 505-3267",
+        address: "745 Judge Street, Herald, Utah, 9914",
+        about: "Ex eiusmod et laborum commodo labore ex adipisicing ut elit exercitation mollit qui aliquip Lorem. Labore ad laborum culpa ea magna id laboris deserunt proident. Qui et ut quis sunt proident nulla adipisicing nisi elit esse. Proident anim veniam officia sint voluptate aliqua eu anim nulla commodo ex culpa ipsum dolore.\r\n",
+        registered: "2014-07-13T12:58:03 -03:00",
+        latitude: 84.776852,
+        longitude: -144.818219,
+        tags: [
+          "enim",
+          "ea",
+          "eu",
+          "sint",
+          "nostrud",
+          "Lorem",
+          "occaecat"
+        ],
+        friends: [
+          {
+            "id": 0,
+            "name": "Wilcox Mann"
+          },
+          {
+            "id": 1,
+            "name": "Valeria Anthony"
+          },
+          {
+            "id": 2,
+            "name": "Lenora Mason"
+          }
+        ],
+        greeting: "Hello, Ramirez Marsh! You have 9 unread messages.",
+        favoriteFruit: "banana"
+      },
+      {
+        _id: "60e313c9a8ed3d3406604fc3",
+        index: 1,
+        guid: "2b653327-978e-4a8d-9535-7c97c716a2e7",
+        isActive: true,
+        balance: "$3,875.32",
+        picture: "https://picsum.photos/900/600",
+        age: 33,
+        eyeColor: "blue",
+        name: "Burch Frank",
+        gender: "male",
+        company: "RETROTEX",
+        email: "burchfrank@retrotex.com",
+        phone: "+1 (815) 483-2592",
+        address: "572 Hanover Place, Marshall, Mississippi, 7533",
+        about: "Aute sit exercitation consectetur aute enim ea labore ea. Proident pariatur occaecat pariatur occaecat ex sint ad excepteur. Consectetur quis incididunt quis dolor sunt veniam deserunt ullamco nisi. Nisi esse magna incididunt reprehenderit labore aliquip qui.\r\n",
+        registered: "2019-12-05T12:16:23 -03:00",
+        latitude: -35.469933,
+        longitude: 72.292237,
+        tags: [
+          "dolore",
+          "proident",
+          "ipsum",
+          "culpa",
+          "enim",
+          "enim",
+          "deserunt"
+        ],
+        friends: [
+          {
+            id: 0,
+            name: "Bessie Serrano"
+          },
+          {
+            id: 1,
+            name: "Marlene Charles"
+          },
+          {
+            id: 2,
+            name: "Hardin Austin"
+          }
+        ],
+        greeting: "Hello, Burch Frank! You have 1 unread messages.",
+        favoriteFruit: "apple"
+      },
+      {
+        _id: "60e313c9f49d31802833abcd",
+        index: 2,
+        guid: "997be003-08d9-4966-bb96-e7a4662766e9",
+        isActive: false,
+        balance: "$3,945.66",
+        picture: "https://picsum.photos/900/600",
+        age: 38,
+        eyeColor: "green",
+        name: "Dora Burt",
+        gender: "female",
+        company: "MAXIMIND",
+        email: "doraburt@maximind.com",
+        phone: "+1 (904) 590-2978",
+        address: "240 Harrison Place, Allamuchy, Ohio, 4465",
+        about: "Dolore consectetur minim velit velit quis cillum velit ipsum dolore excepteur ad dolore. Enim fugiat enim magna ullamco fugiat culpa eu. Et laboris dolor aliquip ex veniam veniam mollit aute. Esse anim excepteur ipsum velit reprehenderit esse est irure ut eu deserunt. Duis eu velit sit cillum Lorem qui irure elit elit pariatur exercitation ea adipisicing eu.\r\n",
+        registered: "2020-12-23T08:59:37 -03:00",
+        latitude: 87.241262,
+        longitude: 24.195006,
+        tags: [
+          "pariatur",
+          "non",
+          "proident",
+          "elit",
+          "culpa",
+          "veniam",
+          "deserunt"
+        ],
+        friends: [
+          {
+            id: 0,
+            name: "Irene Ortega"
+          },
+          {
+            id: 1,
+            name: "Dotson Leach"
+          },
+          {
+            id: 2,
+            name: "Farrell Jacobson"
+          }
+        ],
+        greeting: "Hello, Dora Burt! You have 8 unread messages.",
+        favoriteFruit: "banana"
+      },
+      {
+        _id: "60e313c9dbe717c32bfca2b1",
+        index: 3,
+        guid: "9e2b509d-b15e-4f50-ae54-68f6ffd70205",
+        isActive: true,
+        balance: "$2,555.41",
+        picture: "https://picsum.photos/900/600",
+        age: 31,
+        eyeColor: "green",
+        name: "Katheryn Ryan",
+        gender: "female",
+        company: "MAGNEATO",
+        email: "katherynryan@magneato.com",
+        phone: "+1 (924) 455-3903",
+        address: "200 Brooklyn Avenue, Edgar, Minnesota, 3957",
+        about: "Tempor laboris voluptate nulla irure reprehenderit ut incididunt. Reprehenderit ullamco ipsum consectetur amet proident incididunt minim nulla. Nisi labore consequat tempor ipsum aute nulla sit laboris occaecat dolore. Consequat velit do ipsum aliqua ea fugiat et eiusmod voluptate. Incididunt nulla minim duis proident laboris. Dolore ipsum enim velit incididunt id duis excepteur.\r\n",
+        registered: "2018-02-24T08:02:41 -03:00",
+        latitude: 81.601683,
+        longitude: 9.976401,
+        tags: [
+          "commodo",
+          "ex",
+          "exercitation",
+          "pariatur",
+          "non",
+          "exercitation",
+          "proident"
+        ],
+        friends: [
+          {
+            id: 0,
+            name: "Peck Fuller"
+          },
+          {
+            id: 1,
+            name: "Moore Durham"
+          },
+          {
+            id: 2,
+            name: "Bender Hoffman"
+          }
+        ],
+        greeting: "Hello, Katheryn Ryan! You have 8 unread messages.",
+        "favoriteFruit": "strawberry"
+      }
+    ]
+  }
+}
